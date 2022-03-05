1d21cd4c3c9305ea63e2815f06c84b8c086c50be
Eugenio Paolantonio (g7) <me@medesimo.eu>
2022-03-05 22:52:29 +0100

    [packaging] Untangle apispecifc metapackages
    Previously, apispecific metapackages for newer api levels would
    depend on the old ones, to avoid duplicating dependencies between
    multiple Package blocks.
    
    This worked great on Q (API 29), but it's pretty inflexible for
    the needs of R (API 30).
    
    Let's move the debian/control generation to a specific script, which
    allows to combine multiple dependencies around. This MUST be the source
    of truth of the metapackages contents - i.e. you should modify this script
    and run it to update the debian/control contents.
    
    Apispecific packages are thus now untangled, and a device shouldn't have
    packages for different apilevels installed. Every apispecific packages
    now specify a Provides field for the given variant.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

---
+++ debian/gencontrol.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 Eugenio "g7" Paolantonio <me@medesimo.eu>
+
+# TODO: Maybe use python-debian?
+
+# Common packages for every api level
+COMMON_PACKAGES = [
+	"droidian-quirks-writable-image",
+	"android-base-passwd",
+	"android-base-files",
+	"udev",
+	"lxc-android",
+	"libhybris",
+	"libhybris-utils",
+	"phosh-config-hwcomposer",
+	"udev-config-hybris",
+	"hadess-sensorfw-proxy",
+	"droidian-quirks-hybris-gl",
+	"droidian-quirks-qt-force-gles",
+	"droidian-quirks-tls-padding",
+	"droidian-quirks-xtables-legacy",
+	"gstreamer1.0-droid",
+	"package-sideload",
+	"flash-bootimage",
+	"droidian-quirks-device",
+	"flashlightd",
+	"flatpak-hybris",
+	"flatpak-extension-gl-hybris",
+	"flatpak-extension-gl-hybris-tls-padding",
+]
+
+# Common packages for phones
+COMMON_PHONE_PACKAGES = [
+	"adaptation-hybris (= ${binary:Version})",
+	"ofono",
+	"ofono-scripts",
+	"ofono2mm",
+]
+
+# Devtools packages
+COMMON_DEVTOOLS_PACKAGES = [
+	"adaptation-hybris (= ${binary:Version})",
+	"libhybris-test",
+	"hybris-usb",
+]
+
+# Common packages for api levels 16+
+COMMON_16_PACKAGES = [
+	"adaptation-hybris (= ${binary:Version})",
+	"pulseaudio-config-droid",
+]
+
+# Common packages for api levels 16 through 29 (4.1 to 29)
+COMMON_16_29_PACKAGES = [
+	"pulseaudio-modules-droid-jb2q",
+]
+
+# Common packages for api levels 26+ (8+)
+COMMON_26_PACKAGES = [
+	"bluebinder",
+	"timekeeper",
+]
+
+# Common phone packages for api levels 26+ (8+)
+COMMON_26_PHONE_PACKAGES = [
+	"adaptation-hybris (= ${binary:Version})",
+	"adaptation-hybris-phone (= ${binary:Version})",
+	"adaptation-hybris-api%(level)s (= ${binary:Version})",
+	"ofono-ril-binder-plugin",
+	"ofono-config-ril-binder",
+	"pulseaudio-modules-droid-hidl",
+	"audiosystem-passthrough",
+]
+
+# Common phone packages for dual sim devices for api levels 26+ (8+)
+COMMON_26_DUAL_SIM_PACKAGES = [
+	"adaptation-hybris-api%(level)d-phone (= ${binary:Version})",
+	"ofono-config-ril-binder-dual-sim",
+]
+
+SUPPORTED_APILEVELS = {
+	0 : {
+		"standard" : COMMON_PACKAGES,
+		"phone"    : COMMON_PHONE_PACKAGES,
+		"devtools" : COMMON_DEVTOOLS_PACKAGES,
+	},
+	28 : {
+		"standard"       : COMMON_16_PACKAGES + COMMON_16_29_PACKAGES + COMMON_26_PACKAGES,
+		"phone"          : COMMON_26_PHONE_PACKAGES,
+		"phone-dual-sim" : COMMON_26_DUAL_SIM_PACKAGES,
+	},
+	29 : {
+		"standard"       : COMMON_16_PACKAGES + COMMON_16_29_PACKAGES + COMMON_26_PACKAGES,
+		"phone"          : COMMON_26_PHONE_PACKAGES,
+		"phone-dual-sim" : COMMON_26_DUAL_SIM_PACKAGES,
+	},
+}
+
+PROVIDES_TEMPLATE = """
+Provides: adaptation-hybris-apispecific-%(variant)s
+Conflicts: adaptation-hybris-apispecific-%(variant)s"""
+
+TEMPLATE = """
+Package: %(package)s
+Architecture: any
+Depends: %(depends)s
+Description: %(summary)s
+%(description)s
+"""
+
+if __name__ == "__main__":
+	with open("debian/control.head", "r") as f:
+		head = f.read()
+
+	with open("debian/control", "w") as f:
+		f.write(head)
+
+		for level, variants in SUPPORTED_APILEVELS.items():
+			for variant, depends in variants.items():
+				subs = {
+					"basepkg" : "adaptation-hybris-api%d" % level if level > 0 else "adaptation-hybris",
+					"level" : level,
+					"variant" : variant,
+				}
+				if level > 0:
+					f.write(PROVIDES_TEMPLATE % {"variant" : variant})
+				f.write(
+					TEMPLATE % {
+						"package" : "%(basepkg)s-%(variant)s" % subs if variant != "standard" else subs["basepkg"],
+						"depends" : ",\n         ".join([dep % subs for dep in depends]),
+						"summary" : "Base metapackage for %(basepkg)s adaptations (%(variant)s variant)" % subs,
+						"description" : " This package depends on the required packages\n to allow Droidian work on API %d devices." % level
+						                if level > 0 else
+						                " This package depends on the required packages\n to allow Droidian work on Android devices."
+					}
+				)
